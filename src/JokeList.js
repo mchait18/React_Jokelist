a11d965581479ecd5b72a2b872ad7e416c08cd9b
mchait <miriamchait@gmail.com>
2024-02-29 12:09:13 -0500

    change to useeffect

--- src/JokeList.js
+++ src/JokeList.js
@@ -10,44 +10,46 @@ function JokeList({ numJokesToGet = 5 }) {
   const [jokes, setJokes] = useLocalStorageState('jokes', [])
   const [isLoading, setIsLoading] = useState(true)
 
-  /* retrieve jokes from API */
-  async function getJokes() {
-    try {
-      // load jokes one at a time, adding not-yet-seen jokes
-      let jokes = [];
-      let seenJokes = new Set();
 
-      while (jokes.length < numJokesToGet) {
-        let res = await axios.get("https://icanhazdadjoke.com", {
-          headers: { Accept: "application/json" }
-        });
-        let { ...joke } = res.data;
-
-        if (!seenJokes.has(joke.id)) {
-          seenJokes.add(joke.id);
-          jokes.push({ ...joke, votes: 0 });
-        } else {
-          console.log("duplicate found!");
-        }
-      }
-      setJokes(jokes)
-      setIsLoading(false)
-
-    } catch (err) {
-      console.error(err);
-    }
-  }
   /* at mount, get jokes */
   useEffect(() => {
-    if (!jokes)
+    /* retrieve jokes from API */
+    async function getJokes() {
+      try {
+        // load jokes one at a time, adding not-yet-seen jokes
+        let jokes = [];
+        let seenJokes = new Set();
+
+        while (jokes.length < numJokesToGet) {
+          let res = await axios.get("https://icanhazdadjoke.com", {
+            headers: { Accept: "application/json" }
+          });
+          let { ...joke } = res.data;
+
+          if (!seenJokes.has(joke.id)) {
+            seenJokes.add(joke.id);
+            jokes.push({ ...joke, votes: 0 });
+          } else {
+            console.log("duplicate found!");
+          }
+        }
+        setJokes(jokes)
+        setIsLoading(false)
+
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    if (jokes.length === 0) {
       getJokes()
+    }
     else setIsLoading(false)
-  }, [])
+  }, [jokes, numJokesToGet])
 
   /* empty joke list, set to loading state, and then call getJokes */
   function generateNewJokes() {
+    setJokes([])
     setIsLoading(true)
-    getJokes();
   }
 
   /* change vote for this id by delta (+1 or -1) */
